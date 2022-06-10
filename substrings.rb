08df1c7f17e31ddcc812f9561791896aecb076e7
lemur <noctiphobic@gmail.com>
2022-06-10 11:58:43 -0500

    Convert string to symbol for interfacing with hash

--- substrings.rb
+++ substrings.rb
@@ -1,19 +1,30 @@
 
+word_list = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+def substring(words, word_list)
+  #downcase and strip punctuation
+  words = words.downcase
+  punc = ",.!?"
+  words = words.tr(punc, " ")
+  #turn into an array
+  words = words.split(" ")
 
-def substring(word, substr_arr)
-#first, strip punctuation
-punc = ",.!?"
-word = word.tr(punc, " ")
-#turn into an array
-word = word.split(" ")
-#select and reduce
-#first turn word into an array, because it may be many words
-
-#loop through substr array for each element of word array
-
-#select what is a match
-
-#count it as well?
-
-
+  #~~~~~~~~~~~~~
+  return_hash = {}
+  words.each do |word|
+    word_list.each do |word_list_word|
+      if word == word_list_word
+        #find in return hash
+        if return_hash.has_key?(word.to_sym) #found in return hash, increment it
+          return_hash[word.to_sym] += 1
+        else #not found in return hash, add it
+          return_hash[word.to_sym] = 1
+        end
+      end
+    end
+  end
+  return_hash
 end
+
+puts "Type in your word or words"
+words = gets.chomp
+p substring(words, word_list)
