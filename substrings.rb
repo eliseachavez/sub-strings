d79124693bea1f12f590f85547ce38c0e5fbd1a2
lemur <noctiphobic@gmail.com>
2022-06-10 11:12:43 -0500

    Strip word input and turn into array

---
+++ substrings.rb
@@ -0,0 +1,19 @@
+
+
+def substring(word, substr_arr)
+#first, strip punctuation
+punc = ",.!?"
+word = word.tr(punc, " ")
+#turn into an array
+word = word.split(" ")
+#select and reduce
+#first turn word into an array, because it may be many words
+
+#loop through substr array for each element of word array
+
+#select what is a match
+
+#count it as well?
+
+
+end
